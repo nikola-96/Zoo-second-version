49442eb1ca59e68031582ec6c9da4d5fb8c9c2d2
Nikola <nikolaviki@hotmail.com>
2020-03-03 12:38:23 +0100

    zadatak 1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AnimalList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AnimalList from "./components/AnimalList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    AnimalList
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/AnimalList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>HTML Table</h2>
+    <table>
+      <tr v-for="animal in animals" :key="animal.name">
+        <td>{{ animal.species }}</td>
+        <td>{{ animal.name }}</td>
+        <td>{{ animal.date_of_birth }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      animals: [
+        { species: "dog", name: "Pera", date_of_birth: new Date() },
+        { species: "dog", name: "Gojko", date_of_birth: new Date() },
+        { species: "cat", name: "Milunka", date_of_birth: new Date() },
+        { species: "cat", name: "Zile", date_of_birth: new Date() },
+        { species: "cat", name: "Mile", date_of_birth: new Date() },
+        { species: "bird", name: "Joca", date_of_birth: new Date() }
+      ]
+    };
+  }
+};
+</script>
